27be360cf1cab6e526b779bc4c34d55463efb7d7
steve <stev8855@gmail.com>
2024-05-18 01:34:12 -0500

    modify the home screen to accept providers

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,11 @@
-import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
+// StatelessWidget -> ConsumerWidget
+// StatefulWidget -> ConsumerStatefulWidget
+// |-> State<T> -> ConsumerState<T>
 class HomeScreen extends StatelessWidget {
 
   static const String routeName = 'home-screen';
@@ -9,12 +14,53 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Scaffold(
-        body: Center(
-          child: Text( Environment.movieDbKey ),
-        ),
+        body: _HomeView(),
       )
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  // Haciendo el cambio de Stateful a ConsumerStatefulWidget puedo usar el ConsumerState
+  // en otras palabras, puedo usar el "ref" para acceder a los providers
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Accediendo a los providers (solo read porque es un método)
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    if (nowPlayingMovies.isEmpty) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+          subtitle: Text(movie.overview),
+        );
+      }
     );
   }
 }
